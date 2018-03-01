4f4f60f91a554a59be1e3a92d0bbdeaeffa1e7fc
Virk0 <virko.veelmaa@tptlive.ee>
2018-03-01 09:42:29 +0200

    lm

--- WpfList/WpfList/MainWindow.xaml.cs
+++ WpfList/WpfList/MainWindow.xaml.cs
@@ -23,6 +23,30 @@ namespace WpfList
         public MainWindow()
         {
             InitializeComponent();
+
+            //List<TodoItem> items = new List<TodoItem>();
+            //items.Add(new TodoItem() { Title = "Complete this WPF tutorial", Completion = 45 });
+            //items.Add(new TodoItem() { Title = "Learn C#", Completion = 80 });
+            //items.Add(new TodoItem() { Title = "Go to shop", Completion = 100 });
+            //items.Add(new TodoItem() { Title = "Buy stuff", Completion = 10 });
+            //TodoListBox.ItemsSource = items;
+            List<NamesAndAges> people = new List<NamesAndAges>();
+            people.Add(new NamesAndAges() { FirstName = "Jef", LastName = "Macaroni", Age = 12 });
+            people.Add(new NamesAndAges() { FirstName = "Ron", LastName = "Weasly", Age = 65 });
+            people.Add(new NamesAndAges() { FirstName = "Jeb", LastName = "Notsi", Age = 23 });
+            people.Add(new NamesAndAges() { FirstName = "Svenski", LastName = "Fanne", Age = 16 });
+            NamesAndAgesListBox.ItemsSource = people;
         }
+    }
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public int Completion { get; set; }
+    }
+    public class NamesAndAges
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
     }
 }
